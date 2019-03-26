3bcc007c5b8936e04565093251a79c8028f527b1
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-26 12:54:11 -0700

    Did the thing

--- day01/ex02/oddeven.php
+++ day01/ex02/oddeven.php
@@ -1,4 +1,20 @@
 #! /usr/bin/php
 <?php
-echo "DO THE THING\n"
+echo "Enter a number: ";
+while ($f = fgets(STDIN)) {
+    $f = rtrim($f, "\n");
+    if (preg_match('/^-?[0-9]+$/', $f)) {
+        $num = intval($f);
+        echo "The number $num ";
+        if ($num % 2)
+            echo "is odd";
+        else
+            echo "is even";
+        echo "\n";
+    }
+    else
+        echo "'$f' is not a number\n";
+    echo "Enter a number: ";
+}
+echo "^D\n";
 ?>
