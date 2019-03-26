6530e6d0ac32da2ab4535cfb17c4c26cbc704215
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-26 16:03:15 -0700

    ez

--- day01/ex08/ft_is_sort.php
+++ day01/ex08/ft_is_sort.php
@@ -1,4 +1,10 @@
-#! /usr/bin/php
 <?php
-
+function    ft_is_sort($arr) {
+    for ($i = 1; $i < count($arr); ++$i) {
+        if ($arr[$i - 1] > $arr[$i]) {
+            return (0);
+        }
+    }
+    return (1);
+}
 ?>
