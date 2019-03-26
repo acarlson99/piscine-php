41e4fb22d87db084771cbb199e43bc86307ae968
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-26 16:12:02 -0700

    Added test

--- day01/ex08/ft_is_sort.php
+++ day01/ex08/ft_is_sort.php
@@ -7,4 +7,22 @@ function    ft_is_sort($arr) {
     }
     return (1);
 }
+
+// $tab = array("!/@#;^", "42", "Hello World", "hi", "zZzZzZz");
+// if (ft_is_sort($tab))
+//     echo "The array is sorted\n";
+// else
+//     echo "The array is not sorted\n";
+
+// $tab[] = "What are we doing now ?";
+// if (ft_is_sort($tab))
+//     echo "The array is sorted\n";
+// else
+//     echo "The array is not sorted\n";
+
+// $tab = array(1, 2, 2, 4, 7, 8);
+// if (ft_is_sort($tab))
+//     echo "The array is sorted\n";
+// else
+//     echo "The array is not sorted\n";
 ?>
