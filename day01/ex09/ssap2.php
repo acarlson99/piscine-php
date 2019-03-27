db05b5a11df1d9c2c7f677986ea955e4c584e74a
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-03-26 18:09:15 -0700

    Fixed indentation

--- day01/ex09/ssap2.php
+++ day01/ex09/ssap2.php
@@ -1,4 +1,38 @@
 #! /usr/bin/php
 <?php
+function	ft_split(&$str) {
+	$str = explode(" ", preg_replace('/ +/', ' ', trim($str, " ")));
+	sort($str);
+	return ($str);
+}
 
+function	ischar(&$c) {
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+function	isdigit(&$c) {
+	return (c >= '0' && c <= '9');
+}
+
+function	comp_two(&$s1, &$s2) {
+}
+
+function	cmp($s1, $s2) {
+	$s1 = tolower($s1);
+	$s2 = tolower($s2);
+}
+
+$boytable = array();
+for ($i = 1; $i < $argc; ++$i) {
+	$s = ft_split($argv[$i]);
+	foreach($s as &$v)
+		$boytable[$v] = $v;
+}
 ?>
+
+
+/*
+  char
+  number
+  other
+*/
