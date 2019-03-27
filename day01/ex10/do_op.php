4ecb8a1b680f426331bc89db371595ab7156f5cc
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-03-26 22:23:09 -0700

    Not handling errors.  Let's hope that is cool

--- day01/ex10/do_op.php
+++ day01/ex10/do_op.php
@@ -4,4 +4,24 @@ if ($argc != 4) {
 	echo "Incorrect Parameters\n";
 	exit(1);
 }
+
+$a = intval($argv[1]);
+$b = intval($argv[3]);
+switch ($argv[2]) {
+case '+':
+	printf("%d\n", $a + $b);
+	break;
+case '-':
+	printf("%d\n", $a - $b);
+	break;
+case '/':
+	printf("%d\n", $a / $b);
+	break;
+case '*':
+	printf("%d\n", $a * $b);
+	break;
+case '%':
+	printf("%d\n", $a % $b);
+	break;
+}
 ?>
