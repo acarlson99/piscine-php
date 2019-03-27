bad9660aeb1adc990e846269a7107197aea68314
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-03-26 19:12:14 -0700

    Fixed overflow

--- day01/ex02/oddeven.php
+++ day01/ex02/oddeven.php
@@ -2,19 +2,22 @@
 <?php
 echo "Enter a number: ";
 while ($f = fgets(STDIN)) {
-    $f = rtrim($f, "\n");
-    if (preg_match('/^-?[0-9]+$/', $f)) {
-        $num = intval($f);
-        echo "The number $num ";
-        if ($num % 2)
-            echo "is odd";
-        else
-            echo "is even";
-        echo "\n";
-    }
-    else
-        echo "'$f' is not a number\n";
-    echo "Enter a number: ";
+	$f = rtrim($f, "\n");
+	if ($f > "9223372036854775807") {
+		echo "OWO it's soooooo big\n";
+	}
+	else if (preg_match('/^-?[0-9]+$/', $f)) {
+		$num = intval($f);
+		echo "The number $num ";
+		if ($num % 2)
+			echo "is odd";
+		else
+			echo "is even";
+		echo "\n";
+	}
+	else
+		echo "'$f' is not a number\n";
+	echo "Enter a number: ";
 }
 echo "^D\n";
 ?>
