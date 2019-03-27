f1f5d95c82612f443a8277d18c0ffd3eff899a04
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-03-26 21:34:33 -0700

    fuck

--- day01/ex09/ssap2.php
+++ day01/ex09/ssap2.php
@@ -23,55 +23,29 @@ function	rfturn($l, $v)
 	return ($v);
 }
 
+function	assign_type($c)
+{
+	if (ischar($c))
+		return (0);
+	else if (isdigit($c))
+		return (1);
+	else
+		return (2);
+}
+
 function	cmp($s1, $s2)
 {
 	$i = 0;
+	$s1 = strtolower($s1);
+	$s2 = strtolower($s2);
 	while ($i < count($s1) && $i < count($s2) && $s1[$i] === $s2[$i])
 		++$i;
-	if ($i === count($s1) || $i === count($s2))
-		return (count($s1) < count($s2) ? -1 : 1);
-	else if (ischar($s1[$i]))
-	{
-		echo "CHAR_CMP ";
-		echo "$s1 < $s2  -  ";
-		echo "$s1[$i] < $s2[$i] ";
-		$s1 = strtolower($s1);
-		if (ischar($s2[$i]))
-		{
-			$s2 = strtolower($s2);
-			return (rfturn(__LINE__, $s1[$i] < $s2[$i] ? 1 : -1));
-		}
-		return (rfturn(__LINE__, 1));
-	}
-	else if (isdigit($s1[$i]))
-	{
-		echo "DIG_CMP ";
-		echo "$s1 < $s2  -  ";
-		echo "$s1[$i] < $s2[$i] ";
-		if (isdigit($s2[$i]))
-		{
-			return (rfturn(__LINE__, $s1[$i] < $s2[$i] ? 1 : -1));
-		}
-		else if (ischar($s2[$i]))
-		{
-			return (rfturn(__LINE__, -1));
-		}
-		else
-		{
-			return (rfturn(__LINE__, 1));
-		}
-	}
+	$s1t = assign_type($s1[$i]);
+	$s2t = assign_type($s2[$i]);
+	if ($s1t != $s2t)
+		return ($s1t < $s2t ? -1 : 1);
 	else
-	{
-		echo "OTHER_CMP ";
-		echo "$s1 < $s2  -  ";
-		echo "$s1[$i] < $s2[$i] ";
-		if (!ischar($s1[$i]) && !isdigit($s1[$i]))
-		{
-			return (rfturn(__LINE__, 1));
-		}
-		return (rfturn(__LINE__, $s1[$i] < $s2[$i] ? 1 : -1));
-	}
+		return ($s1[$i] < $s2[$i] ? -1 : 1);
 }
 
 $boytable = array();
