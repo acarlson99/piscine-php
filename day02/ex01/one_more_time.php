4ab28a0c915b55fd466dff2f946ef9ebdd58f89d
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-27 12:38:49 -0700

    Fixed most of it

--- day02/ex01/one_more_time.php
+++ day02/ex01/one_more_time.php
@@ -1,25 +1,72 @@
 #! /usr/bin/php
 <?php
+
+function	ft_split($str) {
+	return (explode(" ", preg_replace('/ +/', ' ', trim($str, " "))));
+}
+
 $weekdays = array(
-	0 => 'fuck this',
+	0 => "/^[Ll]undi$/",
+	1 => "/^[Mm]ardi$/",
+	2 => "/^[Mm]ercredi$/",
+	3 => "/^[Jj]eudi$/",
+	4 => "/^[Vv]endredi$/",
+	5 => "/^[Ss]amedi$/",
+	6 => "/^[Dd]imanche$/",
+);
+
+$days_english = array(
+	0 => "Sun",
+	1 => "Mon",
+	2 => "Tue",
+	3 => "Wed",
+	4 => "Thurs",
+	5 => "Fri",
+	6 => "Sat",
 );
 
 $months = array(
-	0 => "/^[Jj]anvier$/",
-	1 => "/^[Ff][eé]vrier$/",
-	2 => "/^[Mm]ars$/",
-	3 => "/^[Aa]vril$/",
-	4 => "/^[Mm]ai$/",
-	5 => "/^[Jj]uin$/",
-	6 => "/^[Jj]uillet$/",
-	7 => "/^[Aa]out$/",
-	8 => "/^[Ss]eptembre$/",
-	9 => "/^[Oo]ctobre$/",
-	10 => "/^[Nn]ovembre$/",
-	11 => "/^[Dd][eé]cembre$/",
+	1 => "/^[Jj]anvier$/",
+	2 => "/^[Ff][eé]vrier$/",
+	3 => "/^[Mm]ars$/",
+	4 => "/^[Aa]vril$/",
+	5 => "/^[Mm]ai$/",
+	6 => "/^[Jj]uin$/",
+	7 => "/^[Jj]uillet$/",
+	8 => "/^[Aa]out$/",
+	9 => "/^[Ss]eptembre$/",
+	10 => "/^[Oo]ctobre$/",
+	11 => "/^[Nn]ovembre$/",
+	12 => "/^[Dd][eé]cembre$/",
 );
-$i = 0;
-while ($i < count($months) && !preg_match($months[$i], $argv[1]))
-	++$i;
-echo $i, "\n";
+
+// TODO: error check argv[1]
+
+$args = ft_split($argv[1]);
+print_r($args);
+
+$wday = 0;
+while ($wday < count($weekdays) && !preg_match($weekdays[$wday], $args[0]))
+	++$wday;
+
+$day = $args[1];
+
+$mon = 1;
+while ($mon < count($months) && !preg_match($months[$mon], $args[2]))
+	++$mon;
+
+$year = $args[3];
+$time = $args[4];
+
+echo 'wday ', $wday, "\n";
+echo 'day ', $day, "\n";
+echo 'mon ', $mon, "\n";
+echo 'year ', $year, "\n";
+echo 'time ', $time, "\n";
+$total = $year . "-" . $mon . "-" . $day . " " . $days_english[$wday] . " " . $time;
+echo "$total\n";
+$date = date_create_from_format('Y-m-d D H:i:s', $total);
+echo $date->format('Y-m-d D H:i:s'), "\n";
+echo $date->format('U'), "\n";
+// TODO: error check datetime
 ?>
