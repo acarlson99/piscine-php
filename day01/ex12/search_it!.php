303f7cbf66a8b8424d279c159ac24f2ab7254ece
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-03-26 23:01:30 -0700

    yeet

---
+++ day01/ex12/search_it!.php
@@ -0,0 +1,12 @@
+#! /usr/bin/php
+<?php
+if ($argc <= 2)
+	exit(1);
+
+for ($i = 2; $i < $argc; ++$i) {
+	list($key, $value) = explode(':', $argv[$i]);
+	$array[$key] = $value;
+}
+if (array_key_exists($argv[1], $array))
+	echo $array[$argv[1]], "\n";
+?>
