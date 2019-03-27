c6064fa130f4e820179aed3685976c06cb63a5d8
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-27 11:21:15 -0700

    yeet

--- day02/ex00/another_world.php
+++ day02/ex00/another_world.php
@@ -3,7 +3,6 @@
 if ($argc < 2) {
 	exit(1);
 }
-$s = preg_replace('/ +/', ' ', trim($argv[1], " "));
-preg_replace('/\s+/', ' ', $s);
+$s = preg_replace('/[ \t]+/', ' ', trim($argv[1], " \t"));
 echo $s, "\n";
 ?>
