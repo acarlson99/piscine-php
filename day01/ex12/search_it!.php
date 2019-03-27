2982d582cfb25cdc62d356957f0d9d4e5ffcde60
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-27 11:03:30 -0700

    Fixed validation

--- day01/ex12/search_it!.php
+++ day01/ex12/search_it!.php
@@ -4,8 +4,10 @@ if ($argc <= 2)
 	exit(1);
 
 for ($i = 2; $i < $argc; ++$i) {
-	list($key, $value) = explode(':', $argv[$i]);
-	$array[$key] = $value;
+	if (preg_match('/.+:.+/', $argv[$i])) {
+		list($key, $value) = explode(':', $argv[$i]);
+		$array[$key] = $value;
+	}
 }
 if (array_key_exists($argv[1], $array))
 	echo $array[$argv[1]], "\n";
