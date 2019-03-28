4374d235046f3dae6d93005052c4dee720aac159
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-28 16:59:33 -0700

    fuck this

--- day03/ex01/phpinfo.php
+++ day03/ex01/phpinfo.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head>
-	<style type="text/css">
-	</style>
-	<title>phpinfo()</title><meta name="ROBOTS" content="NOINDEX,NOFOLLOW,NOARCHIVE" /></head>
-	<body>
-		
-		<!-- php -S localhost:8100 to run server -->
-		
-		<?php
-		phpinfo();
-		?>
-	</body>
-
-</html>
+<?php
+phpinfo();
+?>
