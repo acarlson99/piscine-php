25b51b591c40e307d17328c3343a90883379aedb
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-27 20:51:48 -0700

    NOOOOOOOOOOOOOOOOOOOOOOO WHY IS THIS SO ANNOYING NOOOOOOOOOOOOOOOOOOOOOOO

--- day02/ex02/magnifying_glass.php
+++ day02/ex02/magnifying_glass.php
@@ -1,4 +1,27 @@
 #! /usr/bin/php
 <?php
-
+for ($argnum = 1; $argnum < $argc; ++$argnum) {
+	$contents = file_get_contents($argv[$argnum]);
+	$b = strlen($contents);
+	$start = strpos($contents, "<a");
+	$end = 0;
+	echo substr($contents, $end, $start);
+	if ($start === FALSE)
+		continue ;
+	while ($start < $b)
+	{
+		if ($start === FALSE)
+			break ;
+		$old_start = $start;
+		$end = strpos($contents, "</a>", $start);
+		if ($end === FALSE)
+			break ;
+		echo "-", substr($contents, $start, $end - $start), "\n\n";
+		$start = strpos($contents, "<a", $end);
+		if ($end === FALSE)
+			break ;
+		echo "2", substr($contents, $end, $start), "/2";
+	}
+	echo substr($contents, $end);
+}
 ?>
