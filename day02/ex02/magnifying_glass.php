1f1fe9bc0aa2189e6f21035e0373f0c8fbb6682a
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-27 22:53:14 -0700

    FUCK

--- day02/ex02/magnifying_glass.php
+++ day02/ex02/magnifying_glass.php
@@ -14,10 +14,8 @@ function	mod_content($contents, $start, $end) {
 	$c_start = strpos($contents, ">", $start + 1);
 	$c_end = strpos($contents, "<", $c_start + 1);
 	$contents = substr_replace($contents, strtoupper(substr($contents, $c_start, $c_end - $c_start)), $c_start, $c_end - $c_start);
-	if ($c_end != $end) {
-		$contents = mod_title($contents, $c_end, $end);
-		$contents = mod_content($contents, $c_end, $end);
-	}
+	if ($c_end != $end)
+		return (mod_content(mod_title($contents, $c_end, $end), $c_end, $end));
 	return ($contents);
 }
 
@@ -27,8 +25,7 @@ while (($start = strpos($contents, "<a", $start)) !== FALSE)
 {
 	if (($end = strpos($contents, "</a>", $start)) === FALSE)
 		break ;
-	$contents = mod_title($contents, $start, $end);
-	$contents = mod_content($contents, $start, $end);
+	$contents = (mod_content(mod_title($contents, $start, $end), $start, $end));
 	$start = $end + 1;
 }
 echo $contents;
