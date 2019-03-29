eda0c5ae55baa25d6f8768b6711a15eb83bfde57
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-28 18:43:40 -0700

    b

--- day03/ex03/cookie_crisp.php
+++ day03/ex03/cookie_crisp.php
@@ -1,3 +1,16 @@
 <?php
-setcookie("abcdef");
+// action, name, value
+// set/get/del any any
+switch ($_GET['action']) {
+case "set":
+	setcookie($_GET['name'], $_GET['value']);
+	break ;
+case "get":
+	if ($_COOKIE[$_GET['name']])
+		echo $_COOKIE[$_GET['name']], "\n";
+	break;
+case "del":
+	setcookie($_GET['name'], NULL, time() - 1);
+	break;
+}
 ?>
