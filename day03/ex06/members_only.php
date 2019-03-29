bfe98473c1abe417ad610c2e7159cb1e469663ba
Alexander Carlson <acarlson@e1z3r2p6.42.us.org>
2019-03-28 19:46:58 -0700

    Got it :)

--- day03/ex06/members_only.php
+++ day03/ex06/members_only.php
@@ -1,2 +1,17 @@
 <?php
+//    [PHP_AUTH_USER] => zaz
+//    [PHP_AUTH_PW] => jaimelespetitsponeys
+if ($_SERVER['PHP_AUTH_USER'] == "zaz" && $_SERVER['PHP_AUTH_PW'] == 'jaimelespetitsponeys') {
+	header("Content-Type: image/png");
+	echo "<html><body>\nHello Zaz<br />\n";
+	echo "<img src='data:image/png;base64,";
+	echo base64_encode(file_get_contents("../img/42.png"));
+	echo "'>\n";
+	echo "</body></html>\n";
+}
+else {
+	header("WWW-Authenticate: Basic realm=''Member area''");
+	header("HTTP/1.1 401 Unauthorized");
+	echo "<html><body>That area is accessible for members only</body></html>\n";
+}
 ?>
