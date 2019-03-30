f085a9cd57526521f5b2607de597ce02947d9eba
Alexander Carlson <acarlson@e1z3r6p6.42.us.org>
2019-03-29 19:39:14 -0700

    NEATO

--- day04/ex01/create.php
+++ day04/ex01/create.php
@@ -1,23 +1,34 @@
 <?php
+function	getData($file) {
+	if (file_exists("../private/passwd"))
+		return (unserialize(file_get_contents("../private/passwd")));
+	else
+		return (array());
+}
+
 function	writeToFile($login, $passwd, $file) {
 	if (!file_exists("../private")) {
 		echo "DOING THING\n";
 		mkdir("../private");
 	}
-	if (file_exists("../private/passwd"))
-		$data = unserialize(file_get_contents("../private/passwd"));
-	else
-		$data = array();
+	$data = getData($file);
+	if ($data[$login])
+		return (-1);
 	$data[$login]['login'] = $login;
 	$data[$login]['passwd'] = hash("whirlpool", $passwd);
 	file_put_contents($file, serialize($data));
+	return (0);
 }
 
 if ($_POST['submit'] == 'OK' && $_POST['login'] && $_POST['passwd']) {
-
 	$login = $_POST['login'];
 	$passwd = $_POST['passwd'];
-	writeToFile($login, $passwd, "../private/passwd");
+	if ($login !== "" && !passwd !== "") {
+		if (writeToFile($login, $passwd, "../private/passwd"))
+			echo "ERROR\n";
+		else
+			echo "OK\n";
+	}
 	print_r(unserialize(file_get_contents("../private/passwd")));
 }
 ?>
