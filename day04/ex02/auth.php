bd4fa37097ae97ffa964eb97d2358dc932ebf356
Alexander Carlson <acarlson@e1z3r6p6.42.us.org>
2019-03-29 20:08:24 -0700

    Added function

--- day04/ex02/auth.php
+++ day04/ex02/auth.php
@@ -1,3 +1,18 @@
 <?php
-
+function	auth($login, $passwd) {
+	$hashpw = hash("whirlpool", $passwd);
+	if (file_exists("../private/passwd"))
+		$db = unserialize(file_get_contents("../private/passwd"));
+	else
+		return (FALSE);
+	foreach($db as $key => $val) {
+		if ($key == $login) {
+			if ($val['passwd'] == $hashpw && $val['login'] == $login)
+				return (TRUE);
+			else
+				return (FALSE);
+		}
+	}
+	return (FALSE);
+}
 ?>
