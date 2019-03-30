d8d762da7117cbbdf401967ed50632947e896244
Alexander Carlson <acarlson@e1z3r6p6.42.us.org>
2019-03-29 19:21:00 -0700

    neat

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -20,6 +20,6 @@ if ($_SESSION['passwd'])
 Username: <input type="text" name="login" value="<?php echo $login_value ?>" />
 <br />
 Password: <input type="password" name="passwd" value="<?php echo $passwd_value ?>" />
-<input type="submit" value="OK" />
+<input type="submit" name="submit" value="OK" />
 </form>
 </body></html>
---
+++ day04/ex01/create.php
@@ -0,0 +1,24 @@
+<?php
+function	writeToFile($login, $passwd, $file) {
+	if (!file_exists("../private")) {
+		echo "DOING THING\n";
+		mkdir("../private");
+	}
+	if (file_exists("../private/passwd"))
+		$data = unserialize(file_get_contents("../private/passwd"));
+	else
+		$data = array();
+	$data[$login]['login'] = $login;
+	$data[$login]['passwd'] = hash("whirlpool", $passwd);
+	file_put_contents($file, serialize($data));
+}
+
+if ($_POST['submit'] == 'OK' && $_POST['login'] && $_POST['passwd']) {
+
+	$login = $_POST['login'];
+	$passwd = $_POST['passwd'];
+	writeToFile($login, $passwd, "../private/passwd");
+	print_r(unserialize(file_get_contents("../private/passwd")));
+	return ;
+}
+?>
