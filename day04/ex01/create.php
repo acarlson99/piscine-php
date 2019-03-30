ef61378a6d0a706c1bd2cb80a0c057bf51b863c2
Alexander Carlson <acarlson@e1z3r6p6.42.us.org>
2019-03-29 19:43:36 -0700

    Crushed it

--- day04/ex01/create.php
+++ day04/ex01/create.php
@@ -13,22 +13,28 @@ function	writeToFile($login, $passwd, $file) {
 	}
 	$data = getData($file);
 	if ($data[$login])
-		return (-1);
+		return (1);
 	$data[$login]['login'] = $login;
 	$data[$login]['passwd'] = hash("whirlpool", $passwd);
 	file_put_contents($file, serialize($data));
 	return (0);
 }
 
+$err = 0;
 if ($_POST['submit'] == 'OK' && $_POST['login'] && $_POST['passwd']) {
 	$login = $_POST['login'];
 	$passwd = $_POST['passwd'];
 	if ($login !== "" && !passwd !== "") {
 		if (writeToFile($login, $passwd, "../private/passwd"))
-			echo "ERROR\n";
-		else
-			echo "OK\n";
+			$err = 1;
 	}
-	print_r(unserialize(file_get_contents("../private/passwd")));
+	else
+		$err = 1;
 }
+else
+	$err = 1;
+if ($err)
+	echo "ERROR\n";
+else
+	echo "OK\n";
 ?>
