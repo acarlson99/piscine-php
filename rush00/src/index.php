9d4f76107ae76e686ace542ded694930aa5b430e
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-03-30 12:07:05 -0700

    Added add to cart page thing

--- rush00/src/index.php
+++ rush00/src/index.php
@@ -1,6 +1,8 @@
 <?php
 $items = unserialize(file_get_contents("../private/items"));
 foreach ($items as $item) {
-	echo $item['name'], $item['price'], "<img src='", $item['img'], "'width=25%><br />";
+	echo $item['name'], $item['price'], "<img src='", $item['img'], "'width=25%><br />", "<form action=\"addtocart.php\" name=\"addtocart.php\" method=\"get\" >";
+	echo "<input type=\"submit\" name=\"submit\" value=\"", $item['name'], "\" />";
+	echo "</form><br />";
 }
 ?>
