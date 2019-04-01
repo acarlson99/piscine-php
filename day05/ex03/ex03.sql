1ae83cae377761451effb60241c48bf8b07ad428
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-04-01 16:42:06 -0700

    fix 😳

--- day05/ex03/ex03.sql
+++ day05/ex03/ex03.sql
@@ -1,6 +1,5 @@
 INSERT INTO ft_table (login, `group`, creation_date)
-SELECT login, 'other', birthday
+SELECT last_name, 'other', birthdate
 FROM user_card
-WHERE last_name LIKE "%a%"
-AND LENGTH(login) < 9
+WHERE last_name LIKE "%a%" AND LENGTH(last_name) < 9
 ORDER BY last_name LIMIT 10;
