dbf3cc436af50d217a61526e80d8578ac8d1ae92
Alexander Carlson <acarlson@e1z3r6p20.42.us.org>
2019-04-01 20:38:51 -0700

    Added widdwe biddy fixes owo

--- day05/ex06/ex06.sql
+++ day05/ex06/ex06.sql
@@ -1,3 +1,3 @@
-SELECT * FROM film
+SELECT title, summary FROM film
 WHERE LOWER(summary) LIKE '%vincent%'
 ORDER BY id_film;
--- day05/ex09/ex09.sql
+++ day05/ex09/ex09.sql
@@ -1,4 +1,4 @@
 SELECT COUNT(*)
 AS `nb_short-films`
 FROM film
-WHERE duration < 42;
+WHERE duration <= 42;
