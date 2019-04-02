6f88f7ee424db3fd799f8a106b9763a96e87615e
Alexander Carlson <acarlson@e1z3r6p20.42.us.org>
2019-04-01 22:38:49 -0700

    wat

--- day05/ex16/ex16.sql
+++ day05/ex16/ex16.sql
@@ -1,5 +1,7 @@
 SELECT COUNT(*) AS movies
 FROM member_history
-WHERE (date < '2007-27-07'
-AND date > '2006-30-10')
+WHERE (
+date < '2007-27-07'
+AND date > '2006-30-10'
+)
 OR DATE_FORMAT(date,'%m-%d') = '12-24';
