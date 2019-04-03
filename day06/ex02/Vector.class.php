2df2ca090225c54a8230f4cdd19dcbd26131b2ea
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-04-02 17:23:59 -0700

    WHAT

--- day06/ex02/Vector.class.php
+++ day06/ex02/Vector.class.php
@@ -1,55 +1,36 @@
 <?php
 
+require_once("../ex01/Vertex.class.php");
+
 class	Vector {
 
 	static $verbose = false;
 
-	private $_x;
-	private $_y;
-	private $_z;
-	private $_w = 0.0;
+	private $_orig;
+	private $_dest;
 
 	static function	doc() {
 		return (file_get_contents("Vector.doc.txt"));
 	}
 
 	function	__construct(array $kwargs) {
-		$this->_x = $kwargs['x'];
-		$this->_y = $kwargs['y'];
-		$this->_z = $kwargs['z'];
-		if (array_key_exists('w', $kwargs))
-			$this->_w = $kwargs['w'];
-		if (Vertex::$verbose)
+		$this->_dest = clone $kwargs['dest'];
+		if (array_key_exists('orig', $kwargs))
+			$this->_orig = clone $kwargs['orig'];
+		else
+			$this->_orig = new Vertex(array('x' => 0, 'y' => 0,
+											'z' => 0, 'w' => 1));
+		if (Vector::$verbose)
 			echo "$this constructed\n";
 	}
 
 	function	__destruct() {
-		if (Vertex::$verbose)
+		if (Vector::$verbose)
 			echo "$this destructed\n";
 	}
 
 	function	__toString() {
-		return ("Vertex( x: " . number_format($this->_x, 2) . ", y: " . number_format($this->_y, 2) . ", z:" . number_format($this->_z, 2) . ", w:" . number_format($this->_w, 2) . " )");
-	}
-
-	function	getX() {
-		return ($this->_x);
-	}
-
-	function	getY() {
-		return ($this->_y);
-	}
-
-	function	getZ() {
-		return ($this->_z);
-	}
-
-	function	getW() {
-		return ($this->_w);
-	}
-
-	function	getColor() {
-		return ($this->_color);
+		return ("Vector( x: " . number_format($this->_x, 2) . ", y: " . number_format($this->_y, 2) . ", z:" . number_format($this->_z, 2) . ", w:" . number_format($this->_w, 2) . " )");
 	}
 
 }
