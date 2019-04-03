59c44814699eb00ce74db88c6931441e9b9863a0
Alexander Carlson <acarlson@e1z2r9p48.42.us.org>
2019-04-03 16:53:33 -0700

    I hate it

--- day07/ex00/Tyrion.class.php
+++ day07/ex00/Tyrion.class.php
@@ -1,15 +1,8 @@
 <?php
 
 class Tyrion extends Lannister {
-	public function	__construct() {
-		parent::__construct();
-		print("My name is Tyrion" . PHP_EOL);
-	}
-
-	public function	__destruct() {
-	}
-
 	public function	getSize() {
+		print("My name is Tyrion" . PHP_EOL);
 		return ("Short");
 	}
 
