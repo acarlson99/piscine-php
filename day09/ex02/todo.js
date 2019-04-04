189cb8afa6650c9cc94e4703382fd6888fed1796
Alexander Carlson <acarlson@e1z2r9p48.42.us.org>
2019-04-03 17:03:47 -0700

    *screams of the damned*

--- day09/ex02/todo.js
+++ day09/ex02/todo.js
@@ -1,1 +1,3 @@
-function	
+function	thing() {
+	console.log("wow boy howdy js is amazing huh gee wiz what a doggone treat this is huh fellas im really enjoying myself doing what here im doing this java scipt stuff huh golly gee this is some neato stuff eh lads");
+}
