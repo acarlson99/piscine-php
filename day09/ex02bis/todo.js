277ce0b81aaefcb8ccddc8d49653026e46f03c60
Alexander Carlson <acarlson@e1z2r9p40.42.us.org>
2019-04-05 15:07:14 -0700

    Screw this

--- day09/ex02bis/todo.js
+++ day09/ex02bis/todo.js
@@ -1,20 +1,4 @@
 $('document').ready(function() {
-	function	getCookie(name) {
-		var b = decodeURIComponent(document.cookie);
-		var ca = b.split(';');
-		for (i in ca) {
-			var c = ca[i];
-			if (c.indexOf(name) == 0)
-				return (c.substring(name.length, c.length).split(',').map(x => atob(x)));
-		}
-		return (new Array());
-	}
-
-	function	setCookie(cookme) {
-		var b = cookme.map(x => btoa(x));
-		b = "sampletext=" + b + ";";
-		document.cookie = b;
-	}
 
 	var thing = $.makeArray();
 
@@ -29,23 +13,9 @@ $('document').ready(function() {
 	})
 
 	$('div').click(function(e) {
-		if ($(e.target).hasClass('list_elem')) {
-			console.log("Killing thing");	// TODO: what why does this not work who designed this software anyway
+		if ($(e.target).hasClass('list_elem') && confirm("Remove element '" + $(e.target).text() + "'?")) {
 			$(e.target).remove();
 		}
 	});
 
-	function	ohgodthisisgrossupdateeverythingwhyohgodwhy() {
-		var firstbit = "<div onclick=\"removeElement("
-		var secondbit = ")\">";
-		var thirdbit = "</div>";
-		var div = document.getElementById("ft_list");
-		var i = 0;
-		div.innerHTML = '';
-		for (n in thing) {
-			div.innerHTML = firstbit + i + secondbit + thing[thing.length - n - 1] + thirdbit + div.innerHTML;
-			++i;
-		}
-		document.cookie = thing;
-	}
 });
